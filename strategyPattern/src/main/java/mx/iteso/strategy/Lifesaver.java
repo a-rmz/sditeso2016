b120d54ac81d8ea073008dea45ecf740f68038f4
Fernando Villanueva <jfvs1@hotmail.com>
2016-09-15 07:37:36 -0500

    LifeBets modifications

--- strategyPattern/src/main/java/mx/iteso/strategy/Lifesaver.java
+++ strategyPattern/src/main/java/mx/iteso/strategy/Lifesaver.java
@@ -3,15 +3,28 @@ package mx.iteso.strategy;
 import mx.iteso.strategy.behaviors.BounceBehavior;
 import mx.iteso.strategy.behaviors.DeflateBehavior;
 import mx.iteso.strategy.behaviors.DeformBehaviour;
+import mx.iteso.strategy.behaviors.FlotationBehavior;
 
 public abstract class Lifesaver {
 
     public DeflateBehavior deflateBehavior;
-    public FloatationBehavior floatationBehavior;
+    public FlotationBehavior flotationBehavior;
     public String type;
 
     public Lifesaver() {
 
+        public String performDeflate() {return deflateBehavior.deflate();}
+
+        public String performInflate() { return deflateBehavior.inflate();}
+
+        public String  performFloat() { return flotationBehavior.floatate();}
+
+        public void setDeflateBehavior(DeflateBehavior deflateBehavior) {
+        this.deflateBehavior = deflateBehavior;
+    }
+
+        public void setFloatingBehavior(FlotationBehavior flotationBehavior) {
+        this.flotationBehavior = flotationBehavior;}
 
     }
 }
