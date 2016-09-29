2f81d9a7fab8f20591ee488aca80c751fd6f99d6
Fernando Villanueva <jfvs1@hotmail.com>
2016-09-29 08:35:05 -0500

    Test

--- decoratorPattern/src/main/java/mx/iteso/decorator/Taco.java
+++ decoratorPattern/src/main/java/mx/iteso/decorator/Taco.java
@@ -18,4 +18,5 @@ public abstract class Taco {
             super("Wrong size for this type of taco");
         }
     }
+
 }
---
+++ decoratorPattern/src/test/java/mx/iteso/decorator/condiments/QuesoTest.java
@@ -0,0 +1,35 @@
+package mx.iteso.decorator.condiments;
+
+import mx.iteso.decorator.Taco;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
+
+/**
+ * Created by fernando on 29/09/16.
+ */
+public class QuesoTest {
+    private Taco taco;
+    @Before
+    public void setUp(){
+        taco = mock((Taco.class));
+    }
+
+    @Test
+    public void testCost(){
+        Taco queso = new Queso(taco);
+        when(taco.cost()).thenReturn(8.0);
+        when(taco.getSize()).thenReturn(taco.MINI);
+        double cost = queso.cost();
+        assertEquals(6.5, cost,0);
+    }
+    @Test
+    public void testDescription(){
+        Taco cebolla = new Cebolla(taco);
+        when(taco.getDescription()).thenReturn("Taco Normal");
+        String desc = cebolla.getDescription();
+        assertEquals("Taco Normal con cebolla", desc);
+    }
+}
