3abad2dedb1539792382097d2a18024c6879b1ca
a-rmz <armzprz@gmail.com>
2016-09-29 07:29:09 -0500

    Size method on abstract

--- decoratorPattern/src/main/java/mx/iteso/decorator/Taco.java
+++ decoratorPattern/src/main/java/mx/iteso/decorator/Taco.java
@@ -1,9 +1,15 @@
 package mx.iteso.decorator;
 
 public abstract class Taco {
+    public static final int MINI = 0;
+    public static final int NORMAL = 1;
+    public static final int MEGA = 2;
+    public int size;
+
     public String description = "Any Taco";
     public String getDescription(){
         return description;
     }
     public abstract double cost();
+    public abstract int getSize();
 }
