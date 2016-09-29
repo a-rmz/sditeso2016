1d76201f9ce4836664936c48a68c029a0458220c
a-rmz <armzprz@gmail.com>
2016-09-29 08:35:38 -0500

    Changed TacoNormal size param

--- decoratorPattern/src/main/java/mx/iteso/decorator/tacos/TacoNormal.java
+++ decoratorPattern/src/main/java/mx/iteso/decorator/tacos/TacoNormal.java
@@ -3,7 +3,7 @@ package mx.iteso.decorator.tacos;
 import mx.iteso.decorator.Taco;
 
 public class TacoNormal extends Taco {
-    public TacoNormal(int size){
+    public TacoNormal(Size size){
         description = "Taco normal";
         this.size = size;
     }
