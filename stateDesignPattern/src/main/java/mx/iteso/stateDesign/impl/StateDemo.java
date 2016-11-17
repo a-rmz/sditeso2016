55ed0a291cfddd2396c78254fee800d79770f81a
Fernando Villanueva <jfvs1@hotmail.com>
2016-11-17 08:14:35 -0600

    Main agregate

---
+++ stateDesignPattern/src/main/java/mx/iteso/stateDesign/impl/StateDemo.java
@@ -0,0 +1,31 @@
+package mx.iteso.stateDesign.impl;
+
+import mx.iteso.stateDesign.impl.States.AngryState;
+import mx.iteso.stateDesign.impl.States.HappyState;
+import mx.iteso.stateDesign.impl.States.SadState;
+
+/**
+ * Created by fernando on 17/11/16.
+ */
+public class StateDemo {
+    public static void main() {
+        Context context = new Context();
+
+        AngryState angry= new AngryState();
+        angry.doAction(context);
+
+        System.out.println(context.getState().toString());
+
+        SadState sad = new SadState();
+        sad.doAction(context);
+
+        System.out.println(context.getState().toString());
+
+        HappyState happy = new HappyState();
+        happy.doAction(context);
+
+        System.out.println(context.getState().toString());
+
+    }
+}
+
